423dd5ed3522a0b66f758df572270b02f065e551
Ryan Remaly <remalyryan@gmail.com>
2021-02-20 13:15:27 -0500

    First commit

---
+++ DockPong/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  DockPong
+//
+//  Created by Ryan Remaly on 2/20/21.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
